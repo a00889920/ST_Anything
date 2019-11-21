964c61c3e06f0d1265604b91f1a9ba6a5fdf47e0
Luis <48568681+a00889920@users.noreply.github.com>
2019-11-20 23:15:13 -0800

    Update ST_Anything_Multiples_ESP8266WiFi_Water_PNP.ino

--- Arduino/Sketches/ST_Anything_Multiples_ESP8266WiFi_Water_PNP/ST_Anything_Multiples_ESP8266WiFi_Water_PNP.ino
+++ Arduino/Sketches/ST_Anything_Multiples_ESP8266WiFi_Water_PNP/ST_Anything_Multiples_ESP8266WiFi_Water_PNP.ino
@@ -1,6 +1,6 @@
 //******************************************************************************************
-//  File: ST_Anything_Multiples_ESP8266WiFi.ino
-//  Authors: Dan G Ogorchock & Daniel J Ogorchock (Father and Son)
+//  File: ST_Anything_Multiples_ESP8266WiFi_Water_PNP.ino
+//  Authors: Dan G Ogorchock & Daniel J Ogorchock (Father and Son) and a00889920
 //
 //  Summary:  This Arduino Sketch, along with the ST_Anything library and the revised SmartThings
 //            library, demonstrates the ability of one NodeMCU ESP8266 to
@@ -43,10 +43,25 @@
 #include <Constants.h>       //Constants.h is designed to be modified by the end user to adjust behavior of the ST_Anything library
 #include <Device.h>          //Generic Device Class, inherited by Sensor and Executor classes
 #include <Sensor.h>          //Generic Sensor Class, typically provides data to ST Cloud (e.g. Temperature, Motion, etc...)
+#include <Executor.h>        //Generic Executor Class, typically receives data from ST Cloud (e.g. Switch)
+#include <InterruptSensor.h> //Generic Interrupt "Sensor" Class, waits for change of state on digital input
 #include <PollingSensor.h>   //Generic Polling "Sensor" Class, polls Arduino pins periodically
 #include <Everything.h>      //Master Brain of ST_Anything library that ties everything together and performs ST Shield communications
 
+#include <PS_Illuminance.h> //Implements a Polling Sensor (PS) to measure light levels via a photo resistor
+
+#include <PS_TemperatureHumidity.h> //Implements a Polling Sensor (PS) to measure Temperature and Humidity via DHT library
+#include <PS_DS18B20_Temperature.h> //Implements a Polling Sesnor (PS) to measure Temperature via DS18B20 libraries
+//#include <PS_Water.h>               //Implements a Polling Sensor (PS) to measure presence of water (i.e. leak detector)
 #include <PS_Water_NPN.h>   //Implements a Polling Sensor (PS) to measure presence of water (i.e. leak detector)
+#include <IS_Motion.h>      //Implements an Interrupt Sensor (IS) to detect motion via a PIR sensor
+#include <IS_Contact.h>     //Implements an Interrupt Sensor (IS) to monitor the status of a digital input pin
+#include <IS_Smoke.h>       //Implements an Interrupt Sensor (IS) to monitor the status of a digital input pin
+#include <IS_DoorControl.h> //Implements an Interrupt Sensor (IS) and Executor to monitor the status of a digital input pin and control a digital output pin
+#include <IS_Button.h>      //Implements an Interrupt Sensor (IS) to monitor the status of a digital input pin for button presses
+#include <EX_Switch.h>      //Implements an Executor (EX) via a digital output to a relay
+#include <EX_Alarm.h>       //Implements Executor (EX)as an Alarm Siren capability via a digital output to a relay
+#include <S_TimedRelay.h>   //Implements a Sensor to control a digital output pin with timing capabilities
 
 //*************************************************************************************************
 //NodeMCU v1.0 ESP8266-12e Pin Definitions (makes it much easier as these match the board markings)
@@ -54,37 +69,51 @@
 //#define LED_BUILTIN 16
 //#define BUILTIN_LED 16
 //
-#define D0 16  //no internal pullup resistor
-#define D1  5
-#define D2  4
-#define D3  0  //must not be pulled low during power on/reset, toggles value during boot
-#define D4  2  //must not be pulled low during power on/reset, toggles value during boot
-#define D5 14
-#define D6 12
-#define D7 13
-#define D8 15  //must not be pulled high during power on/reset
+//#define D0 16  //no internal pullup resistor
+//#define D1  5
+//#define D2  4
+//#define D3  0  //must not be pulled low during power on/reset, toggles value during boot
+//#define D4  2  //must not be pulled low during power on/reset, toggles value during boot
+//#define D5 14
+//#define D6 12
+//#define D7 13
+//#define D8 15  //must not be pulled high during power on/reset
 
 //******************************************************************************************
 //Define which Arduino Pins will be used for each device
 //******************************************************************************************
 #define PIN_WATER_1 A0 //NodeMCU ESP8266 only has one Analog Input Pin 'A0'
 
-#define PIN_WATER_NPN_1 D1 // NPN for wather sensor
+#define PIN_WATER_NPN_1 D8 // NPN for wather sensor
+
+#define PIN_ALARM_1 D0       //SmartThings Capabilty "Alarm"
+#define PIN_SWITCH_1 D1      //SmartThings Capability "Switch"
+#define PIN_CONTACT_1 D2     //SmartThings Capabilty "Contact Sensor"
+#define PIN_BUTTON_1 D3      //SmartThings Capabilty Button / Holdable Button (Normally Open!)
+#define PIN_BUTTON_2 D4      //SmartThings Capabilty Button / Holdable Button (Normally Open!)
+#define PIN_MOTION_1 D5      //SmartThings Capabilty "Motion Sensor" (HC-SR501 PIR Sensor)
+#define PIN_SMOKE_1 D6       //SmartThings Capabilty "Smoke Detector"
+#define PIN_TEMPERATURE_1 D7 //SmartThings Capabilty "Temperature Measurement" (Dallas Semiconductor DS18B20)
+//#define PIN_TIMEDRELAY_1 D8  //SmartThings Capability "Relay Switch"
 
 //******************************************************************************************
 //ESP8266 WiFi Information
 //******************************************************************************************
-String str_ssid     = "Frontier0624";                               //  <---You must edit this line!
-String str_password = "8988317121";                                 //  <---You must edit this line!
-IPAddress ip(192, 168, 254, 200);         // Device IP Address      //  <---You must edit this line!
-IPAddress gateway(192, 168, 254, 254);    //router gateway          //  <---You must edit this line!
-IPAddress subnet(255, 255, 255, 0);       //LAN subnet mask         //  <---You must edit this line!
-IPAddress dnsserver(192, 168, 254, 254);  //DNS server              //  <---You must edit this line!
-const unsigned int serverPort = 8090;     // port to run the http server on
+String str_ssid = "yourSSIDhere";             //  <---You must edit this line!
+String str_password = "yourWiFiPasswordhere"; //  <---You must edit this line!
+IPAddress ip(192, 168, 1, 227);               //Device IP Address       //  <---You must edit this line!
+IPAddress gateway(192, 168, 1, 1);            //Router gateway          //  <---You must edit this line!
+IPAddress subnet(255, 255, 255, 0);           //LAN subnet mask         //  <---You must edit this line!
+IPAddress dnsserver(192, 168, 1, 1);          //DNS server              //  <---You must edit this line!
+const unsigned int serverPort = 8090;         // port to run the http server on
 
-// Smartthings Hub Information
-IPAddress hubIp(192, 168, 254, 123);      // smartthings hub ip     //  <---You must edit this line!
-const unsigned int hubPort = 39500;       // smartthings hub port
+// Smarthings Hub Information
+//IPAddress hubIp(192, 168, 1, 149);  // smartthings hub ip       //  <---You must edit this line!
+//const unsigned int hubPort = 39500; // smartthings hub port
+
+// Hubitat Hub Information
+IPAddress hubIp(192, 168, 1, 143);  // hubitat hub ip         //  <---You must edit this line!
+const unsigned int hubPort = 39501; // hubitat hub port
 
 
 //******************************************************************************************
@@ -126,17 +155,33 @@ void setup()
   //           to match your specific use case in the ST Phone Application.
   //******************************************************************************************
   //Polling Sensors
-  static st::PS_Water_NPN sensor1(F("water1"), 1800 /*get data interval in seconds (30 min) */, 2 /* interval before and after getting data to turn on/off sensor in seconds*/, 20 /*offset in seconds*/, PIN_WATER_1, PIN_WATER_NPN_1, 200 /*limit*/);
-  //static st::PS_Water_NPN sensor1(F("water1"), 10 /*get data interval in seconds (30 min) */, 2 /* interval before and after getting data to turn on/off sensor in seconds*/, 20 /*offset in seconds*/, PIN_WATER_1, PIN_WATER_NPN_1, 200 /*limit*/);
-  
+  static st::PS_Water_NPN sensor1(F("water1"), 60 /*get data interval in seconds */, 1 /* interval before and after getting data to turn on/off sensor in seconds*/, 20 /*offset in seconds*/, PIN_WATER_1, PIN_WATER_NPN_1, 200 /*limit*/);
+  static st::PS_DS18B20_Temperature sensor2(F("temperature1"), 15, 0, PIN_TEMPERATURE_1, false, 10, 1);
+
+  //Interrupt Sensors
+  static st::IS_Contact sensor3(F("contact1"), PIN_CONTACT_1, LOW, true);
+  static st::IS_Button sensor4(F("button1"), PIN_BUTTON_1, 1000, LOW, true, 500);
+  static st::IS_Button sensor5(F("button2"), PIN_BUTTON_2, 1000, LOW, true, 500);
+  static st::IS_Motion sensor6(F("motion1"), PIN_MOTION_1, HIGH, false);
+  static st::IS_Smoke sensor7(F("smoke1"), PIN_SMOKE_1, HIGH, true, 500);
+
+  //Special sensors/executors (uses portions of both polling and executor classes)
+  // static st::S_TimedRelay sensor8(F("relaySwitch1"), PIN_TIMEDRELAY_1, LOW, false, 3000, 0, 1);
+
+  //Executors
+  static st::EX_Alarm executor1(F("alarm1"), PIN_ALARM_1, LOW, true);
+  static st::EX_Switch executor2(F("switch1"), PIN_SWITCH_1, LOW, true); //Inverted logic for "Active Low" Relay Board
+
   //*****************************************************************************
   //  Configure debug print output from each main class
   //  -Note: Set these to "false" if using Hardware Serial on pins 0 & 1
   //         to prevent communication conflicts with the ST Shield communications
   //*****************************************************************************
   st::Everything::debug = true;
+  st::Executor::debug = true;
   st::Device::debug = true;
   st::PollingSensor::debug = true;
+  st::InterruptSensor::debug = true;
   st::PS_Water_NPN::debug = true;
 
   //*****************************************************************************
@@ -160,11 +205,20 @@ void setup()
   //Add each sensor to the "Everything" Class
   //*****************************************************************************
   st::Everything::addSensor(&sensor1);
+/*  st::Everything::addSensor(&sensor2);
+  st::Everything::addSensor(&sensor3);
+  st::Everything::addSensor(&sensor4);
+  st::Everything::addSensor(&sensor5);
+  st::Everything::addSensor(&sensor6);
+  st::Everything::addSensor(&sensor7);
+*/  //st::Everything::addSensor(&sensor8);
 
   //*****************************************************************************
   //Add each executor to the "Everything" Class
   //*****************************************************************************
-  
+  st::Everything::addExecutor(&executor1);
+  st::Everything::addExecutor(&executor2);
+
   //*****************************************************************************
   //Initialize each of the devices which were added to the Everything Class
   //*****************************************************************************
